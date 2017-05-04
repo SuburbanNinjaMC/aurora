324ac2474b6921f4ee3d862672a26e7d502b3607
SuburbanNinjaMC <suburbanninja575@gmail.com>
2017-05-03 21:47:45 -0400

    Update fun.js

--- fun.js
+++ fun.js
@@ -6,21 +6,24 @@ javascript:
       alert("Yay! You have a good spirit.");
       var y = prompt("So what now? Want me to type something?");
       var z = window.open("", "z", "width:100px");
-			z.document.write("<body contenteditable='true'><title>Textbox</title>" + y + "</body>");
+      z.document.write("<body contenteditable='true'><title>Textbox</title>" + y + "</body>");
       break;
     case "no":
       javascript:
       var x = prompt("Why are you here?"); 
       switch (x) { 
         case "Because I am": 
-          javascript:alert('Oh. Sassy now, are you?'); 
+          javascript:alert('Oh. Sassy now, are you? You will be punished.'); 
           break; 
+	  var a = new Audio("https://www.youtube.com/watch?v=L0vsJhARsgU")
+	  a.play();
         case "I don't know": 
           javascript:alert('Then why are you here?');
           break; 
         case "help": 
           javascript:alert("Commands: \nBecause I am \nI don't know"); 
           break; 
+		
       }
       break;
   }
