82cee8d3dba18819ce3e74b05856406349d63b3b
SuburbanNinjaMC <suburbanninja575@gmail.com>
2017-05-04 16:57:39 -0400

    Update subaurora.js

--- subaurora.js
+++ subaurora.js
@@ -1,10 +1,24 @@
 javascript:
 alert("Sub Aurora v1. Thank you for clicking.");
 alert("My name is SuburbanNinjaMC, and I created Sub Aurora.");
-ale
+alert("Press 'j' to begin.");
+console.log("Sub Aurora v1 Installed on your page. Thank you for choosing us.");
 document.onkeypress = function(e) {
-  if (e.keyCode = 07) {
+  if (e.keyCode == 74) {
      var a = prompt("Now what may I do for you?");
+     switch (a) {
+       case "nothing":
+         javascript: alert("Ok. Thank you for your assistance.");
+         break;
+       case "wikipedia":
+         javascript: window.open("https://wikipedia.com");
+         break;
+       case "friendly chat":
+         javascript: alert("Oh I'm fine thank you. This one lady decided to go ham when I was driving to the store.");
+         alert("People can be such imbeciles, you know?");
+         alert("I know you do.");
+         break;
+              }
   }
 }
 
