4be6516067320c674260ec4aba4cc34866d06d8f
SuburbanNinjaMC <suburbanninja575@gmail.com>
2017-05-03 21:44:42 -0400

    Create fun.js

---
+++ fun.js
@@ -0,0 +1,29 @@
+javascript:
+  var x = prompt("So you want to have some fun?");
+  switch (x) {
+    case "yes":
+      javascript: 
+      alert("Yay! You have a good spirit.");
+      var y = prompt("So what now? Want me to type something?");
+      var z = window.open("", "z", "width:100px");
+			z.document.write("<body contenteditable='true'><title>Textbox</title>" + y + "</body>");
+      break;
+    case "no":
+      javascript:
+      var x = prompt("Why are you here?"); 
+      switch (x) { 
+        case "Because I am": 
+          javascript:alert('Oh. Sassy now, are you?'); 
+          break; 
+        case "I don't know": 
+          javascript:alert('Then why are you here?');
+          break; 
+        case "help": 
+          javascript:alert("Commands: \nBecause I am \nI don't know"); 
+          break; 
+      }
+      break;
+  }
+  
+      
+   
