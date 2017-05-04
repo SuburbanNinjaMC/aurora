762d1a2efa4cd6472b4e389705fa90f2e0643dd6
SuburbanNinjaMC <suburbanninja575@gmail.com>
2017-05-04 16:52:28 -0400

    Update subaurora.js

--- subaurora.js
+++ subaurora.js
@@ -1,4 +1,14 @@
 javascript:
-alert("Sub Aurora v1 Installed on Page.");
-console.log("Sub Aurora v1 Installed on current Page.");
+alert("Sub Aurora v1. Thank you for clicking.");
+alert("My name is SuburbanNinjaMC, and I created Sub Aurora.");
+ale
+document.onkeypress = function(e) {
+  if (e.keyCode = 07) {
+     var a = prompt("Now what may I do for you?");
+  }
+}
+
+
+  
+
 
