24a649d69c2a30dd76dc820ca896b757075ef77d
SuburbanNinjaMC <suburbanninja575@gmail.com>
2017-05-04 19:03:11 -0400

    Update subaurora.js

--- subaurora.js
+++ subaurora.js
@@ -10,14 +10,23 @@ document.onkeypress = function(e) {
        case "nothing":
          javascript: alert("Ok. Thank you for your assistance.");
          break;
-       case "wikipedia":
-         javascript: window.open("https://wikipedia.com");
+       case "dictionary":
+         javascript: window.open("http://dictionary.com");
          break;
        case "friendly chat":
          javascript: alert("Oh I'm fine thank you. This one lady decided to go ham when I was driving to the store.");
          alert("People can be such imbeciles, you know?");
          alert("I know you do.");
          break;
+       case "help":
+         javascript: alert("Help is on the way!");
+         alert("Commands to use \nnothing \ndictionary \nfriendly chat \ntake me to your leader \nslogin");
+         break;
+       case "take me to your leader":
+         javascript: window.open("http://marsubia.x10.mx/");
+         break;
+       case "prankd":
+         javascript: window.open("
      }
   }
 }
