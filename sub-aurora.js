e30cd81d02291c132c39f6357ae0bef080b78135
SuburbanNinjaMC <suburbanninja575@gmail.com>
2017-05-16 20:55:12 -0400

    Update sub-aurora.js

--- sub-aurora.js
+++ sub-aurora.js
@@ -1,5 +1,5 @@
 javascript:
-alert("Welcome to Sub Aurora 0.11.");
+alert("Welcome to Sub Aurora 0.12.");
 alert("Press 'J (uppercase)' to begin.");
 console.log("Sub Aurora 0.11 Installed on your page. Thank you for choosing us.");
 document.onkeypress = function(e) {
@@ -63,6 +63,7 @@ document.onkeypress = function(e) {
          javascript: alert("No.");
          break;
 	   case (a.indexOf("fun machine") != -1):
+		     javascript: alert("Press Shift+I
 			 break;
        case (a.indexOf("crash") != -1):
          javascript: 
@@ -165,6 +166,49 @@ document.onkeypress = function(e) {
       window.open("", _blank);
     }
   }
+  
+  if (e.keyCode == 73) {
+  	var x = prompt("So you want to have some fun?");
+  	switch (x) {
+    	case "yes":
+      		javascript: 
+      		alert("Yay! You have a good spirit.");
+      		var y = prompt("So what now? Want me to type something?");
+      		switch (y) {
+	        case "yes":
+		      javascript: var y = prompt("OK! Type something!");
+     		      var z = window.open("", "z", "width:100px");
+     		      z.document.write("<body contenteditable='true'><title>Textbox</title>" + y + "</body>");
+		       break;
+	      case "no": 
+		      javascript: alert("ok.");
+		      break;
+	      }   
+      	break;
+    	case "no":
+      		javascript:
+      		var x = prompt("Then why are you here?"); 
+      		switch (x) { 
+        		case "Because I am": 
+          			javascript:alert('Oh. Sassy now, are you? You will be punished.'); 
+          			var a = "You suck balls";
+	  			for (i == 0; i < 20; i++) {
+	          			javascript: var b = window.open("", "b", "width:100px");
+		  			b.document.write("<body contenteditable='true'><title>Y U do dis</title>" + a + "</body>");
+	 			 }   
+	  			break;
+       			case "I don't know": 
+          			javascript:alert('Then why are you here?');
+          			break; 
+        		case "help": 
+          			javascript:alert("Commands: \nBecause I am \nI don't know"); 
+          			break; 
+		
+     			 }
+      		break;
+  	
+		 }}
+  
 }
 
 
