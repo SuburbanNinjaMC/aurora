7af5218a3bd5964a8bf538fca7efdf88dfb39c6b
SuburbanNinjaMC <suburbanninja575@gmail.com>
2017-05-16 22:08:25 -0400

    Create sub-bookmarklet.js

---
+++ sub-bookmarklet.js
@@ -0,0 +1,5 @@
+javascript: (function () { 
+    var jsCode = document.createElement('script'); 
+    jsCode.setAttribute('src', 'https://github.com/SuburbanNinjaMC/sub-aurora/blob/master/sub-aurora.js');                  
+  document.body.appendChild(jsCode); 
+ }());
