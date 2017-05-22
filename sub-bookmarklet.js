9c026ee6da73ec60f02e47735b6f22bcee0373c6
SuburbanNinjaMC <suburbanninja575@gmail.com>
2017-05-22 16:39:17 -0400

    Delete sub-bookmarklet.js

--- sub-bookmarklet.js
+++
@@ -1,5 +0,0 @@
-javascript: (function () { 
-    var jsCode = document.createElement('script'); 
-    jsCode.setAttribute('src', 'http://marsubia.x10.mx/aurora.js');                  
-  document.body.appendChild(jsCode); 
- }());
